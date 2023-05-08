f37979ea4a3b3dd0241ed91d307898493aed2e84
Luka Benković <benkovi.luka3@gmail.com>
2023-05-08 19:59:49 +0200

    logo

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,26 @@
   <v-app>
     <v-app-bar
       app
-      color=""
+      color="black"
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
+
+      </div>
+
+      <v-btn
+        href="/"
+        text
+      >
+      <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          src="@/assets/logorsg.jpg"
+          width="250"
         />
-      </div>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
       </v-btn>
     </v-app-bar>
 
