830ca8639a3f95731cdf443436794e096b28f912
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-05-09 20:34:16 +0200

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,24 +5,24 @@
       grow
       dark
     >
-      <v-btn>
+      <v-btn value="home" to="/home">
         <span>Home</span>
   
         <v-icon>mdi-home</v-icon>
       </v-btn>
   
-      <v-btn>
+      <v-btn value="bmi" to="/bmi">
         <span>BMI</span>
   
         <v-icon>mdi-chart-box</v-icon>
       </v-btn>
   
-      <v-btn>
+      <v-btn value="diary" to="/diary">
         <span>Diary</span>
   
         <v-icon>mdi-application-edit-outline</v-icon>
       </v-btn>
-      <v-btn>
+      <v-btn value="profile" to="/profile">
         <span>Profile</span>
   
         <v-icon>mdi-account</v-icon>
