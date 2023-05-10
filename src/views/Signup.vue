fb0c3caef98ff5a9e7317538f2cddc0759f3f728
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-05-10 17:03:22 +0200

    Create Signup.vue

---
+++ src/views/Signup.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="main" :style="{'background-color':'black', 'height':'100%'}">
+    <div class="header" :style="{'padding-left':'15px'}">
+        <v-btn color="black" :style="{'width':'30px'}">
+            <v-icon
+            x-large
+            color="white"
+            >
+                mdi-chevron-left
+            </v-icon>
+        </v-btn>
+    </div>
+    <h2 :style="{'color':'white','text-align':'center'}">Create your account</h2>
+    <div class="input-signup">
+        <h3 :style="{'color':'white'}">First name</h3>
+        <input type="first-name" class="input-field">
+        <h3 :style="{'color':'white'}">Last name</h3>
+        <input type="last-name" class="input-field">
+        <h3 :style="{'color':'white'}">E-mail</h3>
+        <input type="email" class="input-field">
+        <h3 :style="{'color':'white'}">Password</h3>
+        <input type="password" class="input-field">
+
+        <div class="submit">
+        <v-btn 
+        color="green"
+        :style="btnstyleGreen"
+        to="/home"
+        >
+            Create account
+        </v-btn>
+        </div>
+    </div>
+    </div>    
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  data(){
+    return{
+      btnstyleGreen:{
+        borderRadius: '20px',
+        width: '300px',
+        marginTop: '10px'
+      }
+    }
+  },
+
+}
+
+</script>
+<style>
+.input-signup {
+    margin-top: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+.input-field {
+    background-color: #D29433;
+    width: 300px;
+    min-width: 100px;
+    padding: 8px 16px 8px 16px;
+    border-radius: 20px;
+    text-align: center;
+    margin: 10px;
+}
+.submit{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 30px;
+}
+</style>
