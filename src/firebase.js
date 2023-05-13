3d26749526440dc4e1edbabfcd375a957cd3b0ff
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-05-13 17:44:48 +0200

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-zqO4K_a2r4ITMfETsK_UxA5JVMsYpuY",
+  authDomain: "ready-set-gym.firebaseapp.com",
+  projectId: "ready-set-gym",
+  storageBucket: "ready-set-gym.appspot.com",
+  messagingSenderId: "496201983543",
+  appId: "1:496201983543:web:e435248c3a912b410b01b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
