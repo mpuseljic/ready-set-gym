6fb1d34dda2768fea2bbbbec62d4a25e53688c76
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-05-24 23:40:04 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -52,6 +52,25 @@
   <div class="header">
       <h1 :style="{'color': 'white'}">MY WORKOUT PLAN</h1>
     </div>
+    <div class="add">
+      <v-btn @click="openDialog" color="#D29433" block elevation="2" large small tile x-small>ADD WORKOUT</v-btn>
+
+<v-dialog v-model="dialogVisible" max-width="700px">
+  <v-card color="grey">
+    <v-card-title>Add Workout</v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-file-input v-model="selectedFile" label="Select an image" accept="image/*"></v-file-input>
+        <v-text-field v-model="workoutName" label="Workout Name"></v-text-field>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="#D29433" @click="saveWorkout">Save</v-btn>
+      <v-btn color="secondary" @click="closeDialog">Cancel</v-btn>
+    </v-card-actions>
+  </v-card>
+</v-dialog>
+    </div>
     <ProfileCard v-for="(card, index) in workoutcards" :key="index" :card="card" />
 
     <div class="header">
@@ -70,6 +89,7 @@ import router from '@/router';
 import ProfileCard from '@/components/ProfileCard.vue';
 import ExerciseList from '@/components/ExerciseList.vue';
 import store from "@/store";
+import {firebase, db, storage} from '@/firebase'
 
 
   export default {
@@ -79,6 +99,9 @@ import store from "@/store";
         
       },
     data: () => ({
+      dialogVisible: false,
+      selectedFile: null,
+      workoutName: '',
       model: 0,
       carouselItems: [
         { url: require('@/assets/lowerbody.jpg'), title: 'Lower Body Attack'},
@@ -150,9 +173,56 @@ import store from "@/store";
             console.log("Workout Card: ", workoutCard)
           }
         },
+        openDialog() {
+      this.dialogVisible = true;
+    },
+    closeDialog() {
+      this.dialogVisible = false;
+      this.resetFields();
+    },
+    resetFields() {
+      this.selectedFile = null;
+      this.workoutName = '';
+    },
+    saveWorkout() {
+      // Handle saving the workout logic here
+      const db = firebase.firestore();
+      const storageRef = firebase.storage().ref();
+      const currentUser = firebase.auth().currentUser;
+      const userId = currentUser.uid; // Replace with the actual user ID
 
-      }
+      // Generate a unique file name using a timestamp
+      const timestamp = new Date().getTime();
+      const fileName = `${userId}_${timestamp}`;
+
+      // Create a reference to the storage location
+      const fileRef = storageRef.child(`workoutImages/${fileName}`);
+
+      // Upload the file to storage
+      fileRef.put(this.selectedFile)
+        .then(snapshot => {
+          // Get the download URL of the uploaded file
+          return snapshot.ref.getDownloadURL();
+        })
+        .then(downloadURL => {
+          // Add the workout to the "myworkouts" collection under the user's document
+          return db.collection('users').doc(userId).collection('myworkouts').doc(
+            this.workoutName,
+          ).set({
+            name: this.workoutName,
+            imageUr: downloadURL
+          })
+        })
+        .then(() => {
+          console.log('Workout saved successfully');
+          this.closeDialog();
+        })
+        .catch(error => {
+          console.error('Error saving workout:', error);
+        });
+    }
   }
+};
 
 
 
@@ -169,6 +239,11 @@ import store from "@/store";
  /* Optional: Adjust the height of the container based on your requirements */
 }
 
+.add{
+margin: 50px;
+
+
+}
 
 
 </style>
